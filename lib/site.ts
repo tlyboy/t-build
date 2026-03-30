def9c252569536495aa8c796986df274c6ebc61d
Guany <1084072690@qq.com>
2026-03-30 12:13:22 +0800

    chore: unify site url config

--- lib/site.ts
+++ lib/site.ts
@@ -1,5 +1,11 @@
-export const siteUrl =
-  process.env.NEXT_PUBLIC_SITE_URL ||
-  (process.env.VERCEL_PROJECT_PRODUCTION_URL
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : 'http://localhost:3000')
+export function getSiteUrl(): string {
+  if (process.env.SITE_URL) {
+    return process.env.SITE_URL.replace(/\/$/, '')
+  }
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  }
+  return 'http://localhost:3000'
+}
+
+export const siteUrl = getSiteUrl()
